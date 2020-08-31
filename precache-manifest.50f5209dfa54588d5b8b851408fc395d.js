10f35a9ab21d7a2a01040bc9739736760b495775
Murilo-PC\Murilo <murilowolfart@hotmail.com>
2020-08-30 23:31:15 -0300

    Updates

---
+++ precache-manifest.50f5209dfa54588d5b8b851408fc395d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c073b071fb845d00e74d1897e9879d97",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9e2e058926660de422b6",
+    "url": "/portfolio/static/css/main.0222621e.chunk.css"
+  },
+  {
+    "revision": "766405de9fd28e8f29b1",
+    "url": "/portfolio/static/js/2.2507dd7f.chunk.js"
+  },
+  {
+    "revision": "9f3151ce82798755899e60228f45ecf2",
+    "url": "/portfolio/static/js/2.2507dd7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e2e058926660de422b6",
+    "url": "/portfolio/static/js/main.cace1593.chunk.js"
+  },
+  {
+    "revision": "c054c160e3f6bb7c261b",
+    "url": "/portfolio/static/js/runtime-main.8f19553e.js"
+  },
+  {
+    "revision": "24e0d52e7d9602708554bdaf851eb6d4",
+    "url": "/portfolio/static/media/airacers1.24e0d52e.png"
+  },
+  {
+    "revision": "0f827bc16b653d350234e05636415363",
+    "url": "/portfolio/static/media/airacers2.0f827bc1.png"
+  },
+  {
+    "revision": "2012367462061a53edfe2c2dd4e3ca24",
+    "url": "/portfolio/static/media/bg5.20123674.png"
+  },
+  {
+    "revision": "f9fe0aeeb223e3fdfc74200f78c5448b",
+    "url": "/portfolio/static/media/dd1.f9fe0aee.png"
+  },
+  {
+    "revision": "94f7f0f7f52781fceca44cb9c6fe96a8",
+    "url": "/portfolio/static/media/dd2.94f7f0f7.png"
+  },
+  {
+    "revision": "ba7aa3bbe2b792fe7c86f04c927040d0",
+    "url": "/portfolio/static/media/dd3.ba7aa3bb.png"
+  },
+  {
+    "revision": "fc3acee37189e451fac9acc3ebaa5fa0",
+    "url": "/portfolio/static/media/hcm1.fc3acee3.png"
+  },
+  {
+    "revision": "c403e467332ab0652a50046ad8ebdb04",
+    "url": "/portfolio/static/media/hcm2.c403e467.png"
+  },
+  {
+    "revision": "d7b10304f5e9b7f5f5a5fb6696f1ec3c",
+    "url": "/portfolio/static/media/hcm3.d7b10304.png"
+  },
+  {
+    "revision": "dc17bab6d10f3009367c9ce9023dfda7",
+    "url": "/portfolio/static/media/mc1.dc17bab6.png"
+  },
+  {
+    "revision": "4234fb53a117c273a8e6785b38dc4ea6",
+    "url": "/portfolio/static/media/mc2.4234fb53.png"
+  },
+  {
+    "revision": "fcbaedf8af2bed0a1a6caa372feb2242",
+    "url": "/portfolio/static/media/mc3.fcbaedf8.png"
+  },
+  {
+    "revision": "a33e436aed0548c80039b7fbd3b21b75",
+    "url": "/portfolio/static/media/pd1.a33e436a.png"
+  },
+  {
+    "revision": "1b3329bf02a83cb2f85351a64e6c3ea3",
+    "url": "/portfolio/static/media/pd2.1b3329bf.png"
+  },
+  {
+    "revision": "5d2fdedb61a4b9b1fc9d23eaa63cbc80",
+    "url": "/portfolio/static/media/pd3.5d2fdedb.png"
+  },
+  {
+    "revision": "4620059a11244248b98a0b346a79bee1",
+    "url": "/portfolio/static/media/profile.4620059a.jpg"
+  }
+]);
