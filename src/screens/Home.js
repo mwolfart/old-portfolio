5314d5a5ecd790c01d54d9b71e5c219a9940d963
Murilo-PC\Murilo <murilowolfart@hotmail.com>
2020-08-30 23:13:30 -0300

    Update home text

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,7 +8,7 @@ export const Home = () =>
     <div className="screen-content__main-panel">
         <div className="screen-content__text-panel screen-content__text-panel--left-block">
             <div className="screen-content__text-title">Hello, and welcome to my page!</div>
-            <div className="screen-content__text-description">Here you can find information about me and some projects I have done - most of them college projects (for now), but hopefully there will be more personal projects in the future :)</div>
+            <div className="screen-content__text-description">Here you can find information about me and some projects I have done - most of them college projects (for now), but hopefully there will be more personal projects with better architectures in the future :)</div>
             <div className="screen-content__text-description">You can open each project's description in the toolbar at the left side of the page. Also, if you wish, you can find this project's repository <a href="https://bitbucket.org/mwolfart/own-portfolio/src/master/">here</a>.</div>
             <div className="screen-content__text-title">About me</div>
             <div className="screen-content__text-description">I am a computer science undergraduate who likes front-end stuff and, in the spare time, I like playing games, singing and looking for new music. Despite preferring front-end I also like to explore different areas of programming and learn new technologies.</div>
